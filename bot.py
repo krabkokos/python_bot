9a5cb26ae91c35fec63ba64fe88759f349d35a62
krabkokos <krabkokos@gmail.com>
2023-10-29 14:51:05 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,55 @@
+from aiogram import Bot, Dispatcher, executor, types
+import random
+from dotenv import load_dotenv
+import os
+load_dotenv()
+ak = 'TOKEN'
+bot = Bot(token=str(os.environ.get(ak)))
+
+dp = Dispatcher(bot)
+chat_id = oken = str(os.environ.get('chat_ID'))
+
+
+@dp.message_handler(commands=['start'])
+async def on_message(message: types.Message):
+    await bot.send_message(message.from_user.id, message.chat.id)
+    await bot.send_message(message.from_user.id, "Привет")
+
+
+@dp.message_handler(commands=['d4'])
+async def on_message(message: types.Message):
+    number = random.randint(1, 4)
+    await bot.send_message(chat_id, str(number))
+
+
+@dp.message_handler(commands=['d6'])
+async def on_message(message: types.Message):
+    number = random.randint(1, 6)
+    await bot.send_message(chat_id, str(number))
+
+
+@dp.message_handler(commands=['d8'])
+async def on_message(message: types.Message):
+    number = random.randint(1, 8)
+    await bot.send_message(chat_id, str(number))
+
+
+@dp.message_handler(commands=['d10'])
+async def on_message(message: types.Message):
+    number = random.randint(1, 12)
+    await bot.send_message(chat_id, str(number))
+
+
+@dp.message_handler(commands=['d12'])
+async def on_message(message: types.Message):
+    number = random.randint(1, 12)
+    await bot.send_message(chat_id, str(number))
+
+
+@dp.message_handler(commands=['d20'])
+async def on_message(message: types.Message):
+    number = random.randint(1, 20)
+    await bot.send_message(chat_id, str(number))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
